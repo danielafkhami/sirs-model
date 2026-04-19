e61d0ae33472f4a818899ac421650e7fb39ac6f7
matthewjaroszek <matthewj369@icloud.com>
2026-04-18 21:35:13 -0400

    update

--- params.py
+++ params.py
@@ -1,9 +1,9 @@
 N = 1000            # Total population
 I0, R0 = 1, 0       # Initial infected and recovered
 S0 = N - I0 - R0    # Initial susceptible
-beta = 0.6          # Infectious rate
+beta = 0.3          # Infectious rate
 gamma = 0.1         # Recovery rate
-xi = 0.04           # Immunity loss rate
+xi = 0.00           # Immunity loss rate
 
-T = 200             # Total time (days)
+T = 1000             # Total time (days)
 dt = 0.1            # Time step
--- simulation.py
+++ simulation.py
@@ -11,15 +11,22 @@ values[0] = [S0, I0, R0]
 for i in range(1, steps):
     values[i] = RungeKutta(values[i-1], dt, sirs_system, beta, gamma, xi, N)
 
-# Plotting
-plt.figure(figsize=(10, 6))
-plt.plot(t_axis, values[:, 0], label='Susceptible', color='blue')
-plt.plot(t_axis, values[:, 1], label='Infected', color='red')
-plt.plot(t_axis, values[:, 2], label='Recovered', color='green')
+def plot():
+    # Plotting
+    plt.figure(figsize=(10, 6))
+    plt.plot(t_axis, values[:, 0], label='Susceptible', color='blue')
+    plt.plot(t_axis, values[:, 1], label='Infected', color='red')
+    plt.plot(t_axis, values[:, 2], label='Recovered', color='green')
 
-plt.title(f"SIRS Model Simulation\n$\\beta={beta}, \\gamma={gamma}, \\xi={xi}$")
-plt.xlabel("Time (Days)")
-plt.ylabel("Population")
-plt.grid(alpha=0.3)
-plt.legend()
-plt.show()
+    plt.title(f"SIRS Model Simulation\n$\\beta={beta}, \\gamma={gamma}, \\xi={xi}$")
+    plt.xlabel("Time (Days)")
+    plt.ylabel("Population")
+    plt.grid(alpha=0.3)
+    plt.legend()
+    plt.show()
+
+plot()
+
+print(f'\nSusceptible: {round(values[-1, 0], 2)}')
+print(f'Infected ~ 0: {values[-1, 1] < 1}')
+print(f'Recovered: {round(values[-1, 2], 2)}\n')
