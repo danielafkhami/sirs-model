4027b982f963c75e71ae0ee9bc00f35ab9b6b96f
matthewjaroszek <matthewj369@icloud.com>
2026-05-13 12:57:58 -0400

    update

--- params.py
+++ params.py
@@ -1,10 +1,10 @@
 N = 1000            # Total population
-I0, R0 = 1, 0       # Initial infected and recovered
+I0, R0 = 5, 0       # Initial infected and recovered
 S0 = N - I0 - R0    # Initial susceptible
-beta = 0.3          # Infectious rate
+beta = 0.2          # Infectious rate
 gamma = 0.1         # Recovery rate
-xi = 0.05           # Immunity loss rate
-lambd = 12          # birth rate
+xi = 0.005          # Immunity loss rate
+lambd = 10.5        # birth rate
 mu = 10             # death rate
 
 
